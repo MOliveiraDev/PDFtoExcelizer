40625a1a6fe1eef5177fb8158e5693eb8a60eee3
Oliver.dev <oliveiramaicon16637@gmail.com>
2025-04-27 20:30:40 -0300

    fix:

--- PDFtoExcelizer/src/main/java/com/springboot/PDFtoExcelizer/Writer/ExcelWriter.java
+++ PDFtoExcelizer/src/main/java/com/springboot/PDFtoExcelizer/Writer/ExcelWriter.java
@@ -1,12 +1,40 @@
 package com.springboot.PDFtoExcelizer.Writer;
 
 
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.springframework.stereotype.Component;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+@Component
 public class ExcelWriter {
 
     // Esse metodo deve ser implementado para escrever os dados extraídos do PDF em um arquivo Excel
 
-    public static void writeToExcel(String [] data, String outputFilePath){
+    public static void writeToExcel(String data, String outputFilePath) throws IOException {
+
+        try (Workbook workbook = new XSSFWorkbook()) {
+            Sheet sheet = workbook.createSheet("Extracted Data");
+            String[] lines = data.split("\n");
+
+            for (int i = 0; i < lines.length; i++) {
+                Row row = sheet.createRow(i);
+                String[] cells = lines[i].split("\\s+"); // Split by whitespace
+                for (int j = 0; j < cells.length; j++) {
+                    Cell cell = row.createCell(j);
+                    cell.setCellValue(cells[j]);
+                }
+            }
+
+            try (FileOutputStream fileOut = new FileOutputStream(outputFilePath)) {
+                workbook.write(fileOut);
+            }
+        }
 
     }
 }
