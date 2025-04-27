77a0d4f4387af4b7104cdd8f0233d6150e60c88b
Oliver.dev <oliveiramaicon16637@gmail.com>
2025-04-27 20:27:58 -0300

    fix: code line 29

--- PDFtoExcelizer/src/main/java/com/springboot/PDFtoExcelizer/Convert/PdfToExcelConvert.java
+++ PDFtoExcelizer/src/main/java/com/springboot/PDFtoExcelizer/Convert/PdfToExcelConvert.java
@@ -1,4 +1,47 @@
 package com.springboot.PDFtoExcelizer.Convert;
 
+import com.springboot.PDFtoExcelizer.Reader.PdfReader;
+import com.springboot.PDFtoExcelizer.Writer.ExcelWriter;
+import lombok.Builder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.io.File;
+import java.nio.file.Paths;
+
+@Service
+@Builder
 public class PdfToExcelConvert {
+
+    private static final Logger logger = LoggerFactory.getLogger(PdfToExcelConvert.class);
+
+    private final PdfReader pdfReader;
+    private final ExcelWriter excelWriter;
+
+    @Scheduled(fixedDelay = 2000)
+    public void processPdfFile() {
+        String inputFolderPath = "D:/GitHub/MeuProjeto/Springboot/com.springboot-PDFtoExcelizer/PDFtoExcelizer/PDFtoExcelizer/Convert/";
+        File inputDir = new File(inputFolderPath);
+        File[] files = inputDir.listFiles(dir -> dir.isFile() && dir.getName().endsWith(".pdf"));
+
+        if (files != null && files.length > 0) {
+            for (File file : files) {
+                String pdfFilePath = file.getAbsolutePath();
+                String outputFolderPath = "D:/GitHub/MeuProjeto/Springboot/com.springboot-PDFtoExcelizer/PDFtoExcelizer/PDFtoExcelizer/Convert/Excel/";
+                String outputFilePath = Paths.get(outputFolderPath, file.getName().replace(".pdf", ".xlsx")).toString();
+
+                try {
+                    String extractedText = PdfReader.extractTextFromPdf(pdfFilePath);
+                    ExcelWriter.writeToExcel(extractedText, outputFilePath);
+                    logger.info("PDF file converted to Excel: {}", outputFilePath);
+                } catch (Exception e) {
+                    logger.error("Error processing file {}: {}", pdfFilePath, e.getMessage(), e);
+                }
+            }
+        } else {
+            logger.info("No PDF files found in the input directory.");
+        }
+    }
 }
