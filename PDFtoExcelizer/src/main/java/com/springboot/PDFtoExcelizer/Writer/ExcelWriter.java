af992c36f01187add16059c0a91444692d82bd76
Oliver.dev <oliveiramaicon16637@gmail.com>
2025-04-27 19:35:23 -0300

    feat: new code

--- PDFtoExcelizer/src/main/java/com/springboot/PDFtoExcelizer/Writer/ExcelWriter.java
+++ PDFtoExcelizer/src/main/java/com/springboot/PDFtoExcelizer/Writer/ExcelWriter.java
@@ -1,4 +1,12 @@
 package com.springboot.PDFtoExcelizer.Writer;
 
+
+
 public class ExcelWriter {
+
+    // Esse metodo deve ser implementado para escrever os dados extraídos do PDF em um arquivo Excel
+
+    public static void writeToExcel(String [] data, String outputFilePath){
+
+    }
 }
