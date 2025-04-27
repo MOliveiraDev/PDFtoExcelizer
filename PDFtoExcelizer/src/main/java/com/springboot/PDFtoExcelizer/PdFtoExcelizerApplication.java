4f6575374d2bb128c802c5f5ab8b7071dfa3902f
Oliver.dev <oliveiramaicon16637@gmail.com>
2025-04-27 19:06:15 -0300

    first commit

---
+++ PDFtoExcelizer/src/main/java/com/springboot/PDFtoExcelizer/PdFtoExcelizerApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.PDFtoExcelizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdFtoExcelizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdFtoExcelizerApplication.class, args);
+	}
+
+}
