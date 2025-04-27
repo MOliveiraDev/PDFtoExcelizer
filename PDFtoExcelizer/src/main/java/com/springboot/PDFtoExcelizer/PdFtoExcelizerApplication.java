46507763bc868b052124a2828a30996f379d92b4
Oliver.dev <oliveiramaicon16637@gmail.com>
2025-04-27 20:29:08 -0300

    fix

--- PDFtoExcelizer/src/main/java/com/springboot/PDFtoExcelizer/PdFtoExcelizerApplication.java
+++ PDFtoExcelizer/src/main/java/com/springboot/PDFtoExcelizer/PdFtoExcelizerApplication.java
@@ -2,12 +2,16 @@ package com.springboot.PDFtoExcelizer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.springboot.PDFtoExcelizer")
+@EnableScheduling
 public class PdFtoExcelizerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PdFtoExcelizerApplication.class, args);
 	}
+
+
 
 }
--- PDFtoExcelizer/src/main/java/com/springboot/PDFtoExcelizer/Reader/PdfReader.java
+++ PDFtoExcelizer/src/main/java/com/springboot/PDFtoExcelizer/Reader/PdfReader.java
@@ -4,8 +4,12 @@ import java.io.File;
 import java.io.IOException;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.text.PDFTextStripper;
+import org.springframework.stereotype.Component;
 
+@Component
 public class PdfReader {
+
+    // Esse metodo deve ser implementado para ler o arquivo PDF e extrair o texto
 
     public static String extractTextFromPdf(String filePath) throws IOException {
         PDDocument document = PDDocument.load(new File(filePath));
