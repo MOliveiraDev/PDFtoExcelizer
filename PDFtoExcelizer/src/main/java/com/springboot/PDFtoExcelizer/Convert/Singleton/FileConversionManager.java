5c3f123134bd313ad32152b27d9a9523b94c7e48
Oliver.dev <oliveiramaicon16637@gmail.com>
2025-05-06 14:44:10 -0300

    feat: add Singleton

--- PDFtoExcelizer/src/main/java/com/springboot/PDFtoExcelizer/Convert/Singleton/FileConversionManager.java
+++ PDFtoExcelizer/src/main/java/com/springboot/PDFtoExcelizer/Convert/Singleton/FileConversionManager.java
@@ -1,4 +1,28 @@
 package com.springboot.PDFtoExcelizer.Convert.Singleton;
 
+import com.springboot.PDFtoExcelizer.Convert.Strategy.ConversionStrategy;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
 public class FileConversionManager {
+    private static final Logger logger = LoggerFactory.getLogger(FileConversionManager.class);
+    private ConversionStrategy strategy;
+
+    public FileConversionManager() {
+        logger.info("Initializing FileConversionManager");
+    }
+
+    public void setStrategy(ConversionStrategy strategy) {
+        this.strategy = strategy;
+        logger.info("Strategy set to: {}", strategy.getClass().getSimpleName());
+    }
+
+    public void convertFile(String inputPath, String outputPath) {
+        if (strategy == null) {
+            throw new IllegalStateException("Estratégia de conversão não definida");
+        }
+        strategy.convert(inputPath, outputPath);
+    }
 }
