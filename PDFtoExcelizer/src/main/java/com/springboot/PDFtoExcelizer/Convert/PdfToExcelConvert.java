426514e3febaa5c16f1afa05b5c75d162468af57
Oliver.dev <oliveiramaicon16637@gmail.com>
2025-05-06 14:43:26 -0300

    feat: add PostConstruct

--- PDFtoExcelizer/src/main/java/com/springboot/PDFtoExcelizer/Convert/PdfToExcelConvert.java
+++ PDFtoExcelizer/src/main/java/com/springboot/PDFtoExcelizer/Convert/PdfToExcelConvert.java
@@ -1,24 +1,34 @@
 package com.springboot.PDFtoExcelizer.Convert;
 
-import com.springboot.PDFtoExcelizer.Reader.PdfReader;
-import com.springboot.PDFtoExcelizer.Writer.ExcelWriter;
-import lombok.Builder;
+import com.springboot.PDFtoExcelizer.Convert.Singleton.FileConversionManager;
+import com.springboot.PDFtoExcelizer.Convert.Strategy.Impl.PdfToExcelStrategy;
+import jakarta.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
-
 import java.io.File;
 import java.nio.file.Paths;
 
 @Service
-@Builder
 public class PdfToExcelConvert {
-
     private static final Logger logger = LoggerFactory.getLogger(PdfToExcelConvert.class);
 
-    private final PdfReader pdfReader;
-    private final ExcelWriter excelWriter;
+    private final FileConversionManager conversionManager;
+    private final PdfToExcelStrategy pdfToExcelStrategy;
+
+    @Autowired
+    public PdfToExcelConvert(FileConversionManager conversionManager, 
+                            PdfToExcelStrategy pdfToExcelStrategy) {
+        this.conversionManager = conversionManager;
+        this.pdfToExcelStrategy = pdfToExcelStrategy;
+    }
+
+    @PostConstruct
+    public void init() {
+        conversionManager.setStrategy(pdfToExcelStrategy);
+    }
 
     @Scheduled(fixedDelay = 2000)
     public void processPdfFile() {
@@ -30,15 +40,10 @@ public class PdfToExcelConvert {
             for (File file : files) {
                 String pdfFilePath = file.getAbsolutePath();
                 String outputFolderPath = "Caminho da pasta para salvar os arquivos";
-                String outputFilePath = Paths.get(outputFolderPath, file.getName().replace(".pdf", ".xlsx")).toString();
+                String outputFilePath = Paths.get(outputFolderPath, 
+                    file.getName().replace(".pdf", ".xlsx")).toString();
 
-                try {
-                    String extractedText = PdfReader.extractTextFromPdf(pdfFilePath);
-                    ExcelWriter.writeToExcel(extractedText, outputFilePath);
-                    logger.info("PDF file converted to Excel: {}", outputFilePath);
-                } catch (Exception e) {
-                    logger.error("Error processing file {}: {}", pdfFilePath, e.getMessage(), e);
-                }
+                conversionManager.convertFile(pdfFilePath, outputFilePath);
             }
         } else {
             logger.info("No PDF files found in the input directory.");
